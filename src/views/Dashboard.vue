253bfd05cf737f53b962b49f96929a7e7c987832
LayMui <laymui.toh@taiger.com>
2021-03-25 16:17:24 +0800

    Add flexbox and a different landing page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="text-center">
-    <img contain height=auto width=100% src="../../src/assets/img/welcome_to_sunflower_field.png"/>
+    <img contain height=auto width=100% src="../../src/assets/img/sunflower2.png"/>
   </div>
 </template>
